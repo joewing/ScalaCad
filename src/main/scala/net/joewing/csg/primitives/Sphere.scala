38b70d9f4a6d60aa2778144ceeb07790696ef191
Joe Wingbermuehle <joewing@joewing.net>
2018-06-16 07:54:29 -0700

    Add Sphere.

---
+++ src/main/scala/net/joewing/csg/primitives/Sphere.scala
@@ -0,0 +1,33 @@
+package net.joewing.csg.primitives
+
+import net.joewing.csg.{Facet, Vertex, BSPTree}
+
+case class Sphere(r: Double = 1, slices: Int = 8, stacks: Int = 8) extends Solid {
+
+  private def vertex(x: Double, y: Double): Vertex = {
+    val theta = x * 2.0 * math.Pi / slices
+    val phi = y * math.Pi / stacks
+    Vertex(
+      r * math.cos(theta) * math.sin(phi),
+      r * math.cos(phi),
+      r * math.sin(theta) * math.sin(phi)
+    )
+  }
+
+  def render: BSPTree = {
+    val fs = Vector.tabulate(slices, stacks) { (x, y) =>
+      val v1 = vertex(x, y)
+      val v2 = vertex(x + 1, y)
+      val v3 = vertex(x + 1, y + 1)
+      val v4 = vertex(x, y + 1)
+      if (y == 0) {
+        Vector(Facet(v1, v3, v4))
+      } else if (y == stacks - 1) {
+        Vector(Facet(v1, v2, v4))
+      } else {
+        Vector(Facet(v1, v2, v3), Facet(v1, v3, v4))
+      }
+    }.flatten.flatten
+    BSPTree(fs)
+  }
+}
--- src/main/scala/net/joewing/csg/primitives/package.scala
+++ src/main/scala/net/joewing/csg/primitives/package.scala
@@ -7,6 +7,8 @@ package object primitives {
   def cylinder(length: Double, bottom: Double, top: Double, sides: Int): Renderable =
     Cylinder(length, bottom, top, sides)
 
+  def sphere(r: Double, slices: Int = 8, stacks: Int = 8): Renderable = Sphere(r, slices, stacks)
+
   implicit class RichRenderable(left: Renderable) {
     def |(right: Renderable): Renderable = Union(left, right)
     def &(right: Renderable): Renderable = Intersection(left, right)
