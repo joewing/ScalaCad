cece9093554678a5a4a7c73d578bd92adfd6f168
Joe Wingbermuehle <joewing@joewing.net>
2018-08-11 11:54:28 -0500

    Restore hole example.

--- src/main/scala/net/joewing/scalacad/examples/HoleExample.scala
+++ src/main/scala/net/joewing/scalacad/examples/HoleExample.scala
@@ -5,8 +5,6 @@ import net.joewing.scalacad.primitives._
 
 object HoleExample extends App {
   val obj = (cube(10, 10, 10).centered - cylinder(10, 3, 3, 16).centered) & sphere(6)
-  //val obj = (cube(10, 10, 10).centered - cylinder(10, 3, 3, 5).centered)
-  //val obj = cube(10, 10, 10).centered | cube(20, 5, 5).centered
   io.StlAsciiFileWriter.write(obj, "hole.stl")
-  io.AwtRenderer.show(io.StlAsciiFileReader.read("hole.stl"), showBackfaces = true, showVertices = true)
+  io.AwtRenderer.show(obj)
 }
