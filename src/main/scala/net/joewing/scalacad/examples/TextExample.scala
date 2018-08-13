2d6af5585e605ec8e228d136027bb67f357be992
Joe Wingbermuehle <joewing@joewing.net>
2018-08-12 21:38:33 -0500

    Put the text on an angle.

--- src/main/scala/net/joewing/scalacad/examples/TextExample.scala
+++ src/main/scala/net/joewing/scalacad/examples/TextExample.scala
@@ -6,7 +6,7 @@ import net.joewing.scalacad.primitives._
 object TextExample extends App {
 
   val text = "Hello!"
-  val textDepth = 8
+  val textDepth = 5
   val textObj = Raster.fromText(text, 64).extrude(textDepth).centered
 
   val padding = 16
@@ -15,7 +15,8 @@ object TextExample extends App {
   val boxDepth = boxHeight
   val box = Cube(boxWidth, boxHeight, boxDepth).centered
 
-  val obj = textObj.above(box, overlap = textDepth, Difference.apply)
+  val obj = textObj.above(box, overlap = textDepth, Difference.apply).rotate(x = math.Pi / 4) -
+    cube(boxWidth * 2, boxHeight * 2, boxDepth).centered.translate(z = -boxHeight / 2)
   StlFileWriter.write(obj, "text.stl")
   AwtRenderer.show(obj)
 }
