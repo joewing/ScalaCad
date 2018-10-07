966656e59e3de0e2edbfb59bba17c6226e82a6db
Joe Wingbermuehle <joewing@joewing.net>
2018-10-07 16:32:57 -0500

    Don't force render for rotated bounds.

--- src/main/scala/net/joewing/scalacad/primitives/Rotate.scala
+++ src/main/scala/net/joewing/scalacad/primitives/Rotate.scala
@@ -10,7 +10,18 @@ final case class Rotate[D <: Dim](obj: Primitive[D], x: Double = 0, y: Double = 
   override def transformed(f: Primitive[D] => Primitive[D]): Primitive[D] =
     obj.transformed(o => f(Rotate(o, x, y, z)))
 
-  lazy val minBound: Vertex = reduceVertices(rendered.vertices, _ min _)
-  lazy val maxBound: Vertex = reduceVertices(rendered.vertices, _ max _)
+  private lazy val boundingVertices: Seq[Vertex] = Seq(
+    Vertex(obj.minBound.x, obj.minBound.y, obj.minBound.z),
+    Vertex(obj.maxBound.x, obj.minBound.y, obj.minBound.z),
+    Vertex(obj.minBound.x, obj.maxBound.y, obj.minBound.z),
+    Vertex(obj.maxBound.x, obj.maxBound.y, obj.minBound.z),
+    Vertex(obj.minBound.x, obj.minBound.y, obj.maxBound.z),
+    Vertex(obj.maxBound.x, obj.minBound.y, obj.maxBound.z),
+    Vertex(obj.minBound.x, obj.maxBound.y, obj.maxBound.z),
+    Vertex(obj.maxBound.x, obj.maxBound.y, obj.maxBound.z)
+  ).map(_.rotated(x, y, z))
+
+  lazy val minBound: Vertex = reduceVertices(boundingVertices, _ min _)
+  lazy val maxBound: Vertex = reduceVertices(boundingVertices, _ max _)
 }
 
